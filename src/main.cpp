3fda0d936acdfeb094476ec5f30f232c8656bb48
Rafael Ramildes <rafael.r.ferreira@grad.ufsc.br>
2024-04-09 15:12:30 -0300

    Add: Abstract Scheduler class

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+class AbstractScheduler {
+	public:
+		virtual int swap_context();
+		virtual int get_time_quanta();
+		virtual int set_time_quanta(int quanta);
+};
+
 int main(int argc, char* argv[]){
 	return 0;
 }
