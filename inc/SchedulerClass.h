a3a97ae84ec06e8a2ce204ae31109058843e9efc
Rafael-Ramildes-Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-04-13 11:49:57 -0300

    Fix: Now Scheduler classes inherits from AbstractScheduler and the running context is actually saved

--- inc/SchedulerClass.h
+++ inc/SchedulerClass.h
@@ -12,25 +12,8 @@ class AbstractContex{};
 */
 class AbstractScheduler {
 	public:
-		virtual int swap_context(unsigned int core_number);
 		virtual int add_to_ready(Process* process);
-		virtual unsigned int get_time_quanta(void);
-		virtual int set_time_quanta(int quanta);
-		virtual std::vector<Process*> get_ready_list(void);
-		virtual int set_ready_list(std::vector<Process*> process_vec);
-		virtual Process* get_running_process(void);
-		virtual int set_running_process(Process* process);
-		virtual std::vector<ProcessorCore*> get_cpu_core(void);
-		virtual int set_cpu_core(std::vector<ProcessorCore*> core_vec);
-};
-
-/**
- * @brief Declares all functions of a class to describe a Rate Monotonic Scheduler
-*/
-class RMSScheduler {
-	public:
 		int swap_context(unsigned int core_number);
-		int add_to_ready(Process* process);
 		unsigned int get_time_quanta(void);
 		int set_time_quanta(int quanta);
 		std::vector<Process*> get_ready_list(void);
@@ -48,26 +31,19 @@ class RMSScheduler {
 };
 
 /**
+ * @brief Declares all functions of a class to describe a Rate Monotonic Scheduler
+*/
+class RMSScheduler : public AbstractScheduler {
+	public:
+		int add_to_ready(Process* process) override;
+};
+
+/**
  * @brief Declares all functions of a class to describe a Earliest Deadline First Scheduler
 */
-class EDFScheduler {
+class EDFScheduler : public AbstractScheduler {
 	public:
-		int swap_context(unsigned int core_number);
-		int add_to_ready(Process* process);
-		unsigned int get_time_quanta(void);
-		int set_time_quanta(int quanta);
-		std::vector<Process*> get_ready_list(void);
-		int set_ready_list(std::vector<Process*> process_vec);
-		Process* get_running_process(void);
-		int set_running_process(Process* process);
-		std::vector<ProcessorCore*> get_cpu_core(void);
-		int set_cpu_core(std::vector<ProcessorCore*> core_vec);
-
-	private:
-		int time_quanta;
-		std::vector<Process*> ready_list;
-		Process* running_process;
-		std::vector<ProcessorCore*> cpu_core;
+		int add_to_ready(Process* process) override;
 };
 
 #endif
--- src/SchedulerClass.cpp
+++ src/SchedulerClass.cpp
@@ -1,7 +1,7 @@
 #include "SchedulerClass.h"
 
 
-/* RMS Scheduler methods -----------------------------------------------------*/
+/* Abstract Scheduler methods ------------------------------------------------*/
 /**
  * @brief Exchange the current running context to the next one, according to the
  * 		Rate Monotonic algorithm
@@ -9,7 +9,7 @@
  * @returns id: ID of the running process
  * 			-1: if failed
 */
-int RMSScheduler::swap_context(unsigned int core_number){
+int AbstractScheduler::swap_context(unsigned int core_number){
 	if(ready_list.empty()){
 		return -1;	// Error
 	}
@@ -23,31 +23,22 @@ int RMSScheduler::swap_context(unsigned int core_number){
 	ready_list.erase(ready_list.begin());
 
 	// Swaps Running process with next process
-	RMSScheduler::add_to_ready(running_process);
+	running_process->set_last_context(&running_contex);
+	AbstractScheduler::add_to_ready(running_process);
 	running_process = next_to_run;
 
 	// Loads the next contex
 	std::copy(running_process->get_contex(),&running_contex);
-	get_cpu_core()[core_number]->set_context(&running_contex);
+	AbstractScheduler::get_cpu_core()[core_number]->set_context(&running_contex);
 	
 	return running_process->get_id(); // No error
-}
-
-/**
- * @brief Adds a Process to the ready list in the appropriated position
- * @param process: The Process descriptor object to be added
- * @returns 0: if successfull
- * 			-1: if failed
-*/
-int RMSScheduler::add_to_ready(Process* process){
-	return 0;
 }
 
 /**
  * @brief Gets the time quanta the SO is setup to use
  * @returns time_quanta
 */
-unsigned int RMSScheduler::get_time_quanta(){
+unsigned int AbstractScheduler::get_time_quanta(){
 	return time_quanta;
 }
 
@@ -57,7 +48,7 @@ unsigned int RMSScheduler::get_time_quanta(){
  * @returns 0: if successfull
  * 			-1: if failed
 */
-int RMSScheduler::set_time_quanta(int quanta){
+int AbstractScheduler::set_time_quanta(int quanta){
 	time_quanta = quanta;
 
 	return 0; // No error
@@ -67,7 +58,7 @@ int RMSScheduler::set_time_quanta(int quanta){
  * @brief Gets list of ready Process descriptor object has a vector
  * @returns Vector of the Processes in thr ready state
 */
-std::vector<Process*> RMSScheduler::get_ready_list(){
+std::vector<Process*> AbstractScheduler::get_ready_list(){
 	return ready_list;
 }
 
@@ -86,10 +77,10 @@ std::vector<Process*> RMSScheduler::get_ready_list(){
  * @returns 0: if successfull
  * 			-1: if failed
 */
-int RMSScheduler::set_ready_list(std::vector<Process*> process_vec){
+int AbstractScheduler::set_ready_list(std::vector<Process*> process_vec){
 	for(Process* pP : process_vec){
 		// Ensures the list is sorted
-		RMSScheduler::add_to_ready(pP);
+		AbstractScheduler::add_to_ready(pP);
 	}
 
 	return 0; // No error
@@ -99,7 +90,7 @@ int RMSScheduler::set_ready_list(std::vector<Process*> process_vec){
  * @brief Gets the currently running process
  * @returns Pointer to Process descriptor object of the currently running Process
 */
-Process* RMSScheduler::get_running_process(){
+Process* AbstractScheduler::get_running_process(){
 	return running_process;
 }
 
@@ -109,7 +100,7 @@ Process* RMSScheduler::get_running_process(){
  * @returns 0: if successfull
  * 			-1: if failed
 */
-int RMSScheduler::set_running_process(Process* process){
+int AbstractScheduler::set_running_process(Process* process){
 	running_process = process;
 
 	return 0; // No error
@@ -119,7 +110,7 @@ int RMSScheduler::set_running_process(Process* process){
  * @brief Gets all CPU registered
  * @return A vector of the CPU descriptor object
 */
-std::vector<ProcessorCore*> RMSScheduler::get_cpu_core(){
+std::vector<ProcessorCore*> AbstractScheduler::get_cpu_core(){
 	return cpu_core;
 }
 
@@ -129,45 +120,26 @@ std::vector<ProcessorCore*> RMSScheduler::get_cpu_core(){
  * @returns 0: if successfull
  * 			-1: if failed
 */
-int RMSScheduler::set_cpu_core(std::vector<ProcessorCore*> core_vec){
+int AbstractScheduler::set_cpu_core(std::vector<ProcessorCore*> core_vec){
 	cpu_core = core_vec;
 
 	return 0; // No error
 }
 
 
-/* EDF Scheduler methods -----------------------------------------------------*/
+/* RM Scheduler methods -----------------------------------------------------*/
 /**
- * @brief Exchange the current running context to the next one, according to the
- * 		Rate Monotonic algorithm
- * @param core_number: number of the core which contex has to be swiched
+ * @brief Adds a Process to the ready list in the appropriated position
+ * @param process: The Process descriptor object to be added
  * @returns 0: if successfull
  * 			-1: if failed
 */
-int EDFScheduler::swap_context(unsigned int core_number){
-	if(ready_list.empty()){
-		return -1;	// Error
-	}
-
-	// Save the context object
-	AbstractContex running_contex;
-	std::copy(get_cpu_core()[core_number]->get_context(),&running_contex);
-
-	// Pop next process from the ready list
-	Process *next_to_run = ready_list.front();
-	ready_list.erase(ready_list.begin());
-
-	// Swaps Running process with next process
-	EDFScheduler::add_to_ready(running_process);
-	running_process = next_to_run;
-
-	// Loads the next contex
-	std::copy(running_process->get_contex(),&running_contex);
-	get_cpu_core()[core_number]->set_context(&running_contex);
-	
+int RMSScheduler::add_to_ready(Process* process){
 	return 0; // No error
 }
 
+
+/* EDF Scheduler methods -----------------------------------------------------*/
 /**
  * @brief Adds a Process to the ready list in the appropriated position
  * @param process: The Process descriptor object to be added
@@ -175,89 +147,5 @@ int EDFScheduler::swap_context(unsigned int core_number){
  * 			-1: if failed
 */
 int EDFScheduler::add_to_ready(Process* process){
-	return 0; // No error
-}
-
-/**
- * @brief Gets the time quanta the SO is setup to use
- * @returns time_quanta
-*/
-unsigned int EDFScheduler::get_time_quanta(){
-	return time_quanta;
-}
-
-/**
- * @brief Sets a new time quanta for the SO to use
- * @param quanta: Integer representing the time quanta in a time unit
- * @returns 0: if successfull
- * 			-1: if failed
-*/
-int EDFScheduler::set_time_quanta(int quanta){
-	time_quanta = quanta;
-
-	return 0; // No error
-}
-
-/**
- * @brief Gets list of ready Process descriptor object has a vector
- * @returns Vector of the Processes in thr ready state
-*/
-std::vector<Process*> EDFScheduler::get_ready_list(){
-	return ready_list;
-}
-
-/**
- * @brief Sets a new list of ready Processes
- * 		ATENTION: This will erase the previos list!!
- * @param process_vec: Process descriptor object to be added as a vector
- * @returns 0: if successfull
- * 			-1: if failed
-*/
-int EDFScheduler::set_ready_list(std::vector<Process*> process_vec){
-	for(Process* pP : process_vec){
-		// Ensures the list is sorted
-		EDFScheduler::add_to_ready(pP);
-	}
-
-	return 0; // No error
-}
-
-/**
- * @brief Gets the currently running process
- * @returns Pointer to Process descriptor object of the currently running Process
-*/
-Process* EDFScheduler::get_running_process(){
-	return running_process;
-}
-
-/**
- * @brief Sets a given Process to run
- * @param process: Pointer to the Process descriptor object to be set to run
- * @returns 0: if successfull
- * 			-1: if failed
-*/
-int EDFScheduler::set_running_process(Process* process){
-	running_process = process;
-
-	return 0; // No error
-}
-
-/**
- * @brief Gets all CPU registered
- * @return A vector of the CPU descriptor object
-*/
-std::vector<ProcessorCore*> EDFScheduler::get_cpu_core(){
-	return cpu_core;
-}
-
-/**
- * @brief Sets a new vector of CPUs
- * @param core_vec: CPUs descriptor object as a vector
- * @returns 0: if successfull
- * 			-1: if failed
-*/
-int EDFScheduler::set_cpu_core(std::vector<ProcessorCore*> core_vec){
-	cpu_core = core_vec;
-
 	return 0; // No error
 }
