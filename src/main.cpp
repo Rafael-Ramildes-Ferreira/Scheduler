c3cd81a0d3b2eddc8803b85b485ee59ac54fa7e5
Rafael-Ramildes-Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-04-14 17:41:16 -0300

    Add: basic swaping and printing implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "SchedulerClass.h"
 
 using namespace std;
@@ -21,7 +22,38 @@ class File{
 
 /* Program entry point -------------------------------------------------------*/
 int main(int argc, char* argv[]){
+	// Creates objects
 	RMSScheduler scheduler = new RMSScheduler(1,1);
+	File file;
+	file.read();
+	vector<Process*> all_process = file.get_processes();
+	scheduler.set_ready_list(all_process);
+
+	// Prints diagram header
+
+	// Runs
+	int swaps = 0, time = 0;
+	unsigned int pid = 0;
+	while (pid >= 0){
+		pid = scheduler.swap_context();
+		swaps++;
+		
+		cout << time << "-" << time+1 << "\t";
+		for(Process p: all_process){
+			if(p->get_id() == pid){
+				cout << " ## ";
+			} else if(p->get_creation_time() >= time){
+				cout << " -- ";
+			} else {
+				cout << "    ";
+			}
+		}
+		cout << endl;
+		
+		time++;
+	}
+	
+
 	return 0;
 }
 
